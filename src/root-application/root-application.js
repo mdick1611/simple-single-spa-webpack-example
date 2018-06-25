92f42e7a6ec9e5f782d9531cdf4cc18904aa2a4e
Muffin <marvin.dick96@web.de>
2018-06-25 17:43:03 +0200

    Removed navigation and display all apps at the homepage

--- src/root-application/root-application.js
+++ src/root-application/root-application.js
@@ -1,9 +1,9 @@
 import * as singleSpa from 'single-spa';
 
 singleSpa.registerApplication('app-1', () =>
-  import ('../app1/app1.js'), pathPrefix('/app1'));
+  import ('../app1/app1.js'), () => true);
 singleSpa.registerApplication('app-2', () =>
-  import ('../app2/app2.js'), pathPrefix('/app2'));
+  import ('../app2/app2.js'), () => true);
 
 singleSpa.start();
 
