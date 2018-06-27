0b7b9fffa689200c3aa538031eeb5a06bd9ad2f9
Muffin <marvin.dick96@web.de>
2018-06-27 02:18:15 +0200

    Removed unnecessary dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,7 @@ module.exports = {
   entry: {
     'root-application': 'src/root-application/root-application.js',
     'common-dependencies': [
-      // We want just one version of angular, so we put it into the common dependencies
-      'core-js/client/shim.min.js',
-      '@angular/common',
-      '@angular/compiler',
-      '@angular/core',
-      '@angular/platform-browser-dynamic',
-      '@angular/router',
-      'reflect-metadata',
-      /* Just one version of react, too. react-router is fine to have multiple versions of,
-       * though, so no need to put it in common dependencies
-       */
-      'react',
-      'react-dom',
+      // We want just one version of vue, so we put it into the common dependencies
       'vue',
     ],
   },
@@ -34,10 +22,6 @@ module.exports = {
         test: /\.js?$/,
         exclude: [path.resolve(__dirname, 'node_modules')],
         loader: 'babel-loader',
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
       },
       {
           test: /\.css$/,
